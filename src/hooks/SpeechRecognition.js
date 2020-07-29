53157b9bfe0c7974c6b96aa2a31316ebd15c610b
mertnacakgedigi <mert.nacakgedigi@gmail.com>
2020-07-29 13:52:12 -0700

    props.transition fixed

--- src/hooks/SpeechRecognition.js
+++ src/hooks/SpeechRecognition.js
@@ -5,17 +5,15 @@ import MicIcon from '@material-ui/icons/Mic';
 import IconButton from '@material-ui/core/IconButton'
 
 export default function Speech(props) {
-
     if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
         return null
     };
     
     return (
         <div className="speech">
-          <IconButton><MicIcon className="icon" onClick={SpeechRecognition.startListening}/></IconButton> 
-          <IconButton><StopIcon className="icon" onClick={SpeechRecognition.stopListening}/></IconButton> 
-           
-            <p>Your Voice Command : {props.command ?  props.command : `Waiting for recording`}</p>  
+            <IconButton><MicIcon className="icon" onClick={SpeechRecognition.startListening}/></IconButton> 
+            <IconButton><StopIcon className="icon" onClick={SpeechRecognition.stopListening}/></IconButton> 
+            <p>Your Voice Command : {props.transcript ?  props.transcript : `Waiting for recording`}</p>  
         </div>
     )
 }
