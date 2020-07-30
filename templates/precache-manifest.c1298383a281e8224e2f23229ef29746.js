734ce3be43104253538cd9fc7c9c4dda418c025c
mertnacakgedigi <mert.nacakgedigi@gmail.com>
2020-07-29 19:07:52 -0700

    ready for production

---
+++ templates/precache-manifest.c1298383a281e8224e2f23229ef29746.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26a13fb526f80c147809559dc9c2345c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97ea38dd00235e3fb8c6",
+    "url": "../static/css/main.70e81d44.chunk.css"
+  },
+  {
+    "revision": "2cb34c885280c24ba950",
+    "url": "../static/js/2.40a76c19.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "../static/js/2.40a76c19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97ea38dd00235e3fb8c6",
+    "url": "../static/js/main.bb38738e.chunk.js"
+  },
+  {
+    "revision": "70cfe8bf97cd49d3f67e",
+    "url": "../static/js/runtime-main.0f7d299b.js"
+  },
+  {
+    "revision": "541da1bb0d4fe31dcdec47234b74dd73",
+    "url": "../static/media/canoo.541da1bb.png"
+  },
+  {
+    "revision": "15a18071ef7d4ba70385370e6c6dec0f",
+    "url": "../static/media/canoodark.15a18071.jpg"
+  },
+  {
+    "revision": "6f5879cebff56a6df7ef93bdab72dd91",
+    "url": "../static/media/loop.6f5879ce.gif"
+  }
+]);
---
+++ templates/precache-manifest.efb0f7aa6d3b958f96ae5cefc869a32b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75b9a9b4fae61a7149378934fb67e695",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02e53626383f62bf17f8",
+    "url": "../static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "70452ca2567c34f279f3",
+    "url": "../static/js/2.ec50623a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "../static/js/2.ec50623a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02e53626383f62bf17f8",
+    "url": "../static/js/main.9b4602c1.chunk.js"
+  },
+  {
+    "revision": "70cfe8bf97cd49d3f67e",
+    "url": "../static/js/runtime-main.0f7d299b.js"
+  }
+]);
